8564bd75cb62998f3492d8d20a3dcdac2510bb20
aaron <acyanesswe@gmail.com>
2024-04-29 19:50:10 -0700

    ls p1

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,57 @@
+use std::env;
+use std::fs;
 use std::io;
-//use std::io::*;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
     let mut shell = true;
-    let mut input = String::new();
     while shell {
-        // if user hits enter, then read in whats on current line
-
+        let mut input = String::new();
+        print!(">> ");
+        io::stdout().flush().unwrap();
         io::stdin()
             .read_line(&mut input)
             .expect("error: unable to read user input");
 
+        // clean the input string
+        let input = input.trim();
+
         // create an enum that has potential commands
         // take in user input
         // print user input
-
-        // if a key is pressed, exit program
-        //
-        //if run
+        if input == "ls" {
+            let ls = LsCommand;
+            println!("files/folders {:?}", ls.execute());
+        }
     }
 }
 
-// fn main() {
-//     let mut input = String::new();
-//     io::stdin()
-//         .read_line(&mut input)
-//         .expect("error: unable to read user input");
-//     println!("{}", input);
-// }
+struct LsCommand;
+impl LsCommand {
+    fn execute(&self) -> io::Result<()> {
+        let path = env::current_dir()?;
+        let mut entries = fs::read_dir(path)?
+            .map(|res| res.map(|e| e.path()))
+            .collect::<Result<Vec<_>, io::Error>>()?;
+
+        entries.sort();
+        println!("entries: {:?}", entries);
+
+        for entry in &entries {
+            let new_path = Path::new(entry);
+            let file_stem = new_path.file_stem();
+            print!("{:?}", file_stem.unwrap());
+        }
+        // todo: change return type and remove quotes from result
+
+        Ok(())
+    }
+}
+
+struct CatCommand;
+impl CatCommand {
+    fn execute(&self) {
+        println!("Cat command execute")
+    }
+}
